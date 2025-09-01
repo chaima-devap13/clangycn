0da6107d6b04dc15fc1078c8bd7ca5ddacad84e0
chaima <chaima1306kacem@gmail.com>
2025-09-02 00:50:26 +0100

    modofied files

---
+++ t11.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+ int fact(int *p)
+ {
+    int i,f;
+    f=1;
+    for(i=2;i<=*p;i++)
+    {
+        f=f*i;
+    }
+    return f;
+ }
+ int main () {
+    int n,*e;
+    printf("donner un nombre : \t");
+    scanf("%d",&n);
+    e=NULL;
+    e=malloc(sizeof(*e));
+    *e=n;
+    printf("le factoriel : %d",fact(e) );
+    free(e);
+    return 0;
+
+ }
